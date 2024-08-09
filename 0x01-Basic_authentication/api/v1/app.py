0cb51c34341999e904526c89fccf12e69821d1b5
madiocre <wolvirex@gmail.com>
2024-08-09 18:18:41 +0300

    autherization

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,18 +7,35 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
+@app.before_request
+def auth_user():
+    """Authenticates a user before processing a request.
     """
-    return jsonify({"error": "Not found"}), 404
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
 
 
 @app.errorhandler(401)
@@ -33,6 +50,13 @@ def unauthorized(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,18 +13,22 @@ class Auth():
         pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ blank """
+        """ Method to check of path is excluded.
+        """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        
+
         for paths in excluded_paths:
             if path.rstrip('/') in paths.rstrip('/'):
                 return False
-        
+
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ blank """
+        """ Method to get authorization header.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
