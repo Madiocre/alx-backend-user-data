40ce85de3300cbfe095df9326e8f1321e9f67bc6
madiocre <wolvirex@gmail.com>
2024-08-09 17:26:15 +0300

    auth attempt fix -1

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,25 +2,24 @@
 """
 Auth module for the API
 """
-from api.v1.views import app_views
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
-    """ """
+class Auth():
+    """ Auth Class """
     def __init__(self):
         """ initialize the auth class """
         pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ """
+        """ blank """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ """
+        """ blank """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ """
+        """ blank """
         return None
