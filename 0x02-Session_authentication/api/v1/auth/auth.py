a26ca83f4ccd49c79d43ddefc218eb0262af589c
madiocre <wolvirex@gmail.com>
2024-08-11 01:37:57 +0300

    Session implemnetation

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -35,3 +35,15 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ blank """
         return None
+    def session_cookie(self, request=None):
+        """
+        Returns a cookie from a request
+        Args:
+            request : request object
+        Return:
+            value of _my_session_id cookie from request object
+        """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
