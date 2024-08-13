304492ee138e198133e7694bbe69ac0e06b1e42b
madiocre <wolvirex@gmail.com>
2024-08-13 19:32:58 +0300

    user class edit

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,11 +11,8 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), unique=True, nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        pass
