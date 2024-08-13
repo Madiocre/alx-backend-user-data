94b84bde28931b158a9483371032694f3826b4b8
madiocre <wolvirex@gmail.com>
2024-08-13 20:00:19 +0300

    user fix?

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """
 User class using declarative base
 """
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -16,5 +16,11 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
+
+if __name__ == '__main__':
+    print(User.__tablename__)
+
+    for column in User.__table__.columns:
+        print("{}: {}".format(column, column.type))
