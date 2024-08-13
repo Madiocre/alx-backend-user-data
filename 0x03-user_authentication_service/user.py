b436e316f5a2e9874fca26a48750a2911ae4559e
madiocre <wolvirex@gmail.com>
2024-08-13 07:50:11 +0300

    updated user class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,10 +7,10 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(100), unique=True, nullable=False)
-    password = Column(String(128), nullable=False)
-    session_id = Column(String(128), nullable=True)
-    reset_token = Column(String(128), nullable=True)
+    email = Column(String(250), unique=True, nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
 
     def __repr__(self):
         return f"<User(id={self.id}, username='{self.username}', email='{self.email}')>"
