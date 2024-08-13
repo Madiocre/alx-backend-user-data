74670ef1ab1255830280570d84a2916d8a7e555e
madiocre <wolvirex@gmail.com>
2024-08-13 19:42:55 +0300

    user class edit + style

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,6 +9,8 @@ Base = declarative_base()
 
 
 class User(Base):
+    """ Represents User Table
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
